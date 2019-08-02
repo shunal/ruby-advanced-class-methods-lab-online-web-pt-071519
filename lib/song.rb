7d639842a943bd9a241e3d5adf87357325bab8e0
Steven Hunal <stevehunal@gmail.com>
2019-08-02 22:21:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,7 @@ class Song
    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
   
-  def self.alphabetical()
+  def self.alphabetical
     @@all.sort_by{|x| x.name}
+  end
 end
