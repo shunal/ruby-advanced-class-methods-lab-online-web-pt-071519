865c95b404cb60103dd6fc24047b56ad7fab8d9b
Steven Hunal <stevehunal@gmail.com>
2019-08-02 22:36:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,10 +51,5 @@ class Song
     
   end 
   
-  def self.create_from_filename(name)
-    song = self.new
-    song.name = (file_mp3.split(" - ").chomp(".mp3"))
-    song.artist_name = (file_mp3.split(" - ")[0])
-    self.all << song
-  end
+  
 end
